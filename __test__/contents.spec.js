e5900cd7eab1bba7a1c20f293fe6048d8b79ad70
Chigbogu Orji <brightorji60@gmail.com>
2020-07-16 14:12:19 +0100

    add test

--- __test__/contents.spec.js
+++ __test__/contents.spec.js
@@ -1,9 +1,10 @@
 process.env.NODE_ENV = 'test'
 const supertest = require('supertest')
 const basePath = require('../index')
+const request = supertest(basePath)
 const Contents = require('../contents/contents.model')
 
-describe('TEST API',() => {
+describe('TEST API', () => {
 	beforeEach(async () => {
 		await Contents.deleteMany()
 	})
@@ -13,7 +14,7 @@ describe('TEST DB', () => {
 	it('adds data  to the database', async () => {
 		const content = new Contents({
 			sharerName: 'Neche',
-			contentTitle: 'Hello is also Purity',
+			contentTitle: 'Hello Miss. Purity',
 			contentUrl:
 				'https://baby.hashnode.dev/git-and-github-for-absolute-beginners',
 			contentCategory: 'blog',
@@ -21,8 +22,75 @@ describe('TEST DB', () => {
 
 		await content.save()
 		expect(content.sharerName).toBe('Neche')
-		expect(content.contentTitle).toBe('Hello is also Purity')
-  })
+		expect(content.contentTitle).toBe('Hello Miss. Purity')
+	})
 
-  it()
+	it('retrieve all the contents', async () => {
+		const res = await request.get('/all-resource')
+		expect(res.statusCode).toBe(200)
+	})
+
+	// custom matcher to match array of object returned by database
+	expect.extend({
+		toContainObject(received, expected) {
+			const pass = this.equals(
+				received,
+				expect.arrayContaining(expect.objectContaining(expected))
+			)
+
+			if (pass) {
+				return {
+					message: () =>
+						`expected ${this.utils.printReceived(
+							received
+						)} to be ${this.utils.printExpected(expected)}`,
+					pass: true,
+				}
+			} else {
+				return {
+					message: () =>
+						`expected ${this.utils.printReceived(
+							received
+						)} not to be ${this.utils.printExpected(arguments)}`,
+					pass: false,
+				}
+			}
+		},
+	})
+
+	it('db retrieves files correctly', async () => {
+		const items = [
+			{
+				sharerName: 'Neche',
+				contentTitle: 'some title here',
+				contentCategory: 'article',
+				contentUrl: 'https://neche.com',
+			},
+			{
+				sharerName: 'Spato',
+				contentTitle: 'some title here',
+				contentCategory: 'blog',
+				contentUrl: 'https://spatocode.com/blog',
+			},
+			{
+				sharerName: 'Purity',
+				contentTitle: 'some title here',
+				contentCategory: 'article',
+				contentUrl: 'https://purity.com',
+			},
+		]
+		await Contents.insertMany(items)
+		let content = await Contents.find()
+		expect.toContainObject({
+			sharerName: 'Neche',
+			contentTitle: 'some title here',
+		})
+		expect(content).toHaveLength(4)
+	})
+
+	it('returns empty array if no content is addded', async () => {
+		await Contents.deleteMany()
+		let content = await Contents.find()
+		expect(content).toHaveLength(0)
+	})
 })
